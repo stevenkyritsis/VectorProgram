24aad4ebb27779ad0b9ee824a34082f9a41dc7ba
stevenkyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2018-09-23 19:35:00 -0400

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -4,6 +4,7 @@
 //
 
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 //Vector Structure
@@ -17,9 +18,9 @@ struct Vector {
 
 //Function Prototypes
 void ViewVector();
-void EditVector(double, double);
-void calc_difference(double, double, double)
-void scalar_result(double, double, double)
+double EditVector(double, double);
+void calc_difference(double, double)
+void scalar_result(double, double)
 void scalar_prod(double, double)
 void magnitude(double)
 
@@ -44,8 +45,9 @@ main() {
 	//Menu
 	cout << "In this program you will be able to view/edit vectors and perform operations\n";
 	cout << "to current values in the vectors. Each time the program is restarted the values are\n";
-	cout << "defaulted to (4,9) in Vec1 and (3,-1) in Vec2.\n";
+	cout << "defaulted to (4,9) in Vec1 and (3,-1) in Vec2. Your scalar is also automatically set to 10.\n";
 	cout << endl;
+	cout << "********************MENU**************************" << endl;
 	
 	
 	//Variable for user input of first menu
@@ -60,10 +62,10 @@ main() {
 		switch (input1)
 		{
 			case 1 {
-						do {
+						int input2;
+					do {
 							cout << "Would you like to edit or view your vectors?" << endl;
 							cout << "1: View \t\t 2: Edit" << endl;
-							int input2;
 							cin >> input2;
 						}while(input2 != 1 || input2 != 2)
 						switch (input2)
@@ -72,6 +74,7 @@ main() {
 								ViewVector();
 							}
 							case 2{
+								
 								do {
 									cout << "Which vector would you like to edit? (You can only edit one at a time.)\n";
 									cout << "1: Vec1 \t\t 2: Vec2\n";
@@ -81,10 +84,38 @@ main() {
 								switch (input3)
 								{
 									case 1{
-										EditVector(vec1.x, vec1.y)
+											char ret;
+											do {
+												EditVector(vec1.x, vec1.y)
+												cout << "Would you like to edit the other vector?\n";
+												cin >> ret;
+												if (ret = 'y')
+												{
+													EditVector(vec2.x, vec2.y)
+												}
+												else;
+												cout << "Would you like to go back to the menu?\n";
+												cin >> ret;
+											}while(ret != 'y' || ret != 'm' || ret == NULL)
+										
+												
+											
+
 									}
 									case 2{
-										EditVector(vec2.x, vec2.y)
+										do {
+											EditVector(vec2.x, vec2.y)
+												cout << "Would you like to edit the other vector?\n";
+											cin >> ret;
+											if (ret = 'y')
+											{
+												EditVector(vec1.x, vec1.y)
+											}
+											else;
+											cout << "Would you like to go back to the menu?\n";
+											cin >> ret;
+										} while (ret != 'y' || ret != 'm' || ret == NULL)
+										
 									}
 								}
 								
@@ -93,7 +124,32 @@ main() {
 						}
 			}
 			case 2 {
+				int input4;
 
+				do {
+					cout << "Which operations would you like to perform on the vectors?\n";
+					cout << "1: Vector Subtraction \t\t 2: Scalar Multiplication\n";
+					cout << "3: Scalar Product \t\t 4: Magnitude\n";
+					cin >> input4;
+				} while(input4 != 1 || input4 != 2 || input4 != 3 || input4 != 4)
+				
+					switch (input4)
+					{
+						case 1{
+							calc_difference(vec1, vec2)
+						}
+						case 2{
+							int input5;
+							
+							do {
+								cout << "Which vector would you like to multiply by the scalar?\n";
+								cout << "1: Vec1 \t\t 2: Vec2\n";
+								cin >> input5;
+							}while(input5 != 1 || input5 != 2)
+							
+
+						}
+				}
 			}
 		}
 
